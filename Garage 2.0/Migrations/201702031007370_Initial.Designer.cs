7db20fcff2a480b87c8794fa9326ad8a74ece178
samibhatti <abdulsami.bhatti@gmail.com>
2017-02-03 17:28:23 +0100

    d

--- Garage 2.0/Migrations/201702031007370_Initial.Designer.cs
+++ Garage 2.0/Migrations/201702031007370_Initial.Designer.cs
@@ -7,13 +7,13 @@ namespace Garage_2._0.Migrations
     using System.Resources;
     
     [GeneratedCode("EntityFramework.Migrations", "6.1.3-40302")]
-    public sealed partial class Initial2 : IMigrationMetadata
+    public sealed partial class Initial : IMigrationMetadata
     {
-        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial2));
+        private readonly ResourceManager Resources = new ResourceManager(typeof(Initial));
         
         string IMigrationMetadata.Id
         {
-            get { return "201702030527314_Initial2"; }
+            get { return "201702031007370_Initial"; }
         }
         
         string IMigrationMetadata.Source
