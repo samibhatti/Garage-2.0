a1863c82a9d6ac294427dc23a5ca7c9245111a85
Sir Mimer <sirmimer@hotmail.com>
2017-02-10 16:22:00 +0100

    Add parkinglots and dropdown on check in

--- Garage 2.0/Helpers/parkingHelper.cs
+++ Garage 2.0/Helpers/parkingHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Garage_2._0.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,7 +8,7 @@ namespace Garage_2._0.Helpers
 {
     public static class parkingHelper
     {
-        public static int numberOfLots = 60;
+        public static int numberOfLots = 30;
         public static int pricePerHour = 60;
         public static List<int> accptedTyres = new List<int>() { 2, 4 };
 
@@ -29,5 +30,24 @@ namespace Garage_2._0.Helpers
             int amount = ((24 * pricePerHour) * days) + (hours * pricePerHour) + (minuts / pricePerHour);
             return amount;
         }
+
+        public static List<string> parkingLots = new List<string>()
+        {
+            "A1","A2","A3","A4","A5","A6","A7","A8","A9","A10",
+            "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10",
+            "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10"
+        };
+
+        public static List<string> getFreeParkingLots(List<Vehicle> vehicles)
+        {
+            List<string> freeParkings = parkingLots;
+            foreach(var vehicle in vehicles)
+            {
+                if (freeParkings.Contains(vehicle.ParkingLotNo))
+                    freeParkings.Remove(vehicle.ParkingLotNo);
+            }
+            return freeParkings;
+        }
+
     }
 }
