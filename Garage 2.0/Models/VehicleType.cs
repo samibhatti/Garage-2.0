811b56d64ba3e43d03e56e54c0b290824a594112
Sami <sami@gmail.com>
2017-02-20 15:07:15 +0100

    create detailviewmodel

--- Garage 2.0/Models/VehicleType.cs
+++ Garage 2.0/Models/VehicleType.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace Garage_2._0.Models
     public class VehicleType
     {
         public int Id { get; set; }
+        [Display(Name = "Vehicle Type")]
         public string Name { get; set; }
 
         public virtual ICollection<Vehicle> Vehicles { get; set; }
